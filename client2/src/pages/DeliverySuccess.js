1aab73246bd63af93c6d81c6fff30f196ce955cf
Priyankaa2503 <priyankaar25@gmail.com>
2024-02-17 21:24:19 +0530

    payment

--- client2/src/pages/DeliverySuccess.js
+++ client2/src/pages/DeliverySuccess.js
@@ -1,63 +1,55 @@
-import ParentFrame from "../components/ParentFrame";
+import { Link } from "react-router-dom";
 
 const DeliverySuccess = () => {
   return (
-    <div className="w-full relative rounded-13xl bg-tint-1 overflow-hidden flex flex-col items-center justify-start gap-[117px]">
+    <div className="w-full h-screen relative rounded-13xl bg-tint-1 overflow-hidden flex flex-col items-center justify-start gap-[117px]">
       <section className="self-stretch flex flex-col items-center justify-start gap-[9px]">
-        {/* <header className="self-stretch h-[43.3px] overflow-hidden shrink-0 flex flex-row items-start justify-between pt-4 px-5 pb-[15px] box-border">
-          <div className="flex flex-col items-center justify-start">
+       
+      </section>
+      <section className="self-stretch flex flex-row items-start justify-start pt-0 px-5 pb-[61px] text-center text-5xl text-shade-4 font-label-l2">
+        <div className="flex-1 flex flex-col items-center justify-start gap-[24px]">
+          <div className="w-40 h-40 rounded-61xl bg-tint-2 flex flex-row items-center justify-center">
             <img
-              className="w-[28.4px] h-[11.1px] relative"
+              className="h-[104px] w-[104px] relative rounded-61xl"
               loading="eager"
               alt=""
-              src="/941.svg"
+              src="/confetti.svg"
             />
           </div>
-          <img
-            className="h-[30px] w-[219px] relative object-cover hidden"
-            alt=""
-            src="/notch@2x.png"
-          />
-          <div className="h-[11px] flex flex-row items-end justify-start gap-[5px]">
-            <img
-              className="h-[10.7px] w-[17px] relative"
-              loading="eager"
-              alt=""
-              src="/mobile-signal.svg"
-            />
-            <img
-              className="h-[11px] w-[15.3px] relative min-h-[11px]"
-              loading="eager"
-              alt=""
-              src="/wifi.svg"
-            />
-            <img
-              className="h-[11.3px] w-[24.3px] relative"
-              alt=""
-              src="/battery.svg"
-            />
-          </div>
-          <img
-            className="h-1.5 w-1.5 relative hidden"
-            alt=""
-            src="/recording-indicator.svg"
-          />
-        </header> */}
-        <div className="self-stretch flex flex-row items-center justify-start py-0 px-5">
-          <div className="h-11 w-11 rounded-3xl box-border flex flex-row items-center justify-start py-2.5 pr-[11px] pl-[9px] border-[1px] border-solid border-tint-3">
-            <img
-              className="h-6 w-6 relative overflow-hidden shrink-0"
-              loading="eager"
-              alt=""
-              src="/close--24--outline.svg"
-            />
+          <div className="self-stretch flex flex-col items-center justify-start gap-[32px]">
+            <div className="self-stretch flex flex-col items-center justify-start py-0 px-3 gap-[6px]">
+              <h3 className="m-0 h-[33px] relative text-inherit font-semibold font-inherit inline-block">
+                Order successfully placed
+              </h3>
+              <div className="self-stretch h-14 relative text-base tracking-[0.01em] leading-[28px] inline-block text-tint-7">
+                <span>{`Your order `}</span>
+                <span className="font-medium text-shade-4">#33-A45E</span>
+                <span>
+                  {" "}
+                  has been successfully processed
+                </span>
+              </div>
+            </div>
+            <div className="self-stretch flex flex-col items-center justify-start gap-[24px] text-left text-mid text-white">
+              <Link to="/map" className="w-[300px] no-underline  text-white decoration-none">
+              <div className="w-full rounded-13xl bg-accent flex flex-row items-center justify-center py-[22px] px-[9px]">
+                <div className="relative tracking-[0.01em] leading-[20px] font-semibold">
+                  View ETA
+                </div>
+              </div>
+                  </Link>
+                  <Link to="/home" className="w-[300px] no-underline  text-white decoration-none">
+              <div className="rounded-11xl flex flex-row items-center justify-center text-accent">
+                <div className="relative tracking-[0.01em] leading-[20px] font-semibold">
+                  Continue Munching
+                </div>
+              </div>
+                  </Link>
+            </div>
           </div>
         </div>
       </section>
-      <ParentFrame />
-      <footer className="self-stretch h-[34px] relative">
-        <div className="absolute top-[calc(50%_-_2px)] left-[calc(50%_-_60px)] rounded-81xl bg-shade-4 w-[120px] h-1" />
-      </footer>
+   
     </div>
   );
 };

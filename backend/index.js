db56fed071952b925d023c2967185dfa5f1af964
RacHiT101 <rachitgala05@gmail.com>
2024-02-17 15:30:02 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use("/order", orderRoute);
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });

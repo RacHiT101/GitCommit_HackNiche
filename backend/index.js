560a718f392d82ba4bd3baa505aa45a02c1ea675
AryanGupta2406 <aryanworkgp2406@gmail.com>
2024-02-18 05:11:56 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -43,32 +43,3 @@ app.use("/review", reviewRoute);
 
 
 
-
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(` User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with Id: ${socket.id} has joined the room ${data}`)
-  });
-
-  socket.on("send_message", (data)=>{
-    socket.to(data.room).emit("receive_message", data);
-  })
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
-});
-
-server.listen(5002, () => {
-  console.log("SERVER CHALU HAI BHAI");
-});

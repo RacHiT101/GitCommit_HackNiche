38383c6c5f0143bb8b77e2592e9d16dd3182e24b
AryanGupta2406 <aryanworkgp2406@gmail.com>
2024-02-18 02:09:30 +0530

    Inventory management done

--- admin/src/views/admin/budget/components/TaskCard.jsx
+++ admin/src/views/admin/budget/components/TaskCard.jsx
@@ -13,7 +13,7 @@ import {
   Input,
 } from "@chakra-ui/react";
 
-const TaskCard = ({ icon, title, subtitle, previousExpenses }) => {
+const TaskCard = ({ icon, title, AvailableStocksSum,UsedStockSum }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [todoDetails, setTodoDetails] = useState("");
   const [expenseAmount, setExpenseAmount] = useState("");
@@ -26,28 +26,33 @@ const TaskCard = ({ icon, title, subtitle, previousExpenses }) => {
     onClose(); // Close the modal after saving
   };
 
+  
+
   return (
-    <div className="rounded-md border p-4 bg-white shadow-md">
+    <div className="rounded-md w-max border p-4 bg-white shadow-md">
       <div>
         <h3 className="text-lg font-semibold">{title}</h3>
-        <div className=" flex items-center gap-2">
+        <div className=" flex items-center gap-2 py-2">
           <div className="rounded-full bg-lightPrimary p-3 dark:bg-navy-700">
             <span className="flex items-center text-brand-500 dark:text-white">
               {icon}
             </span>
           </div>
-          <p className="text-gray-900">{subtitle}</p>
+          <div>
+        <p>Total Available Stock: {AvailableStocksSum}</p>
+        <p>Total Used Stock: {UsedStockSum}</p>
+      </div>
         </div>
-        <hr className="my-3" />
+        {/* <hr className="my-3" /> */}
         
 
         {/* Button to open the modal for entering new expenses */}
-        <button
+        {/* <button
           onClick={onOpen}
           className="mt-2 rounded-md bg-blue-500 py-1 px-2 text-white"
         >
           Add Expense
-        </button>
+        </button> */}
       </div>
 
     </div>

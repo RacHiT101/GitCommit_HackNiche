58548c5a726a4b26d4c4d749889040cce2febce8
Priyankaa2503 <priyankaar25@gmail.com>
2024-02-17 15:27:36 +0530

    backend apis

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,37 +1,46 @@
 const mongoose = require('mongoose')
 const orderSchema = new mongoose.Schema(
-    {
-        userId:{
-            type:String,
-            required:true,
-
-        },
-        products:[
-            {
-            productId:{
-                type:String
-            },
-            quantity:{
-                type:Number,
-                default:1
-            },
-
-        }
-
-        ],
-        amount:{
-            type:Number,
-            required:true
-        },
-        address:{
-            type: Object,
-            required:true
-        },
-        status:{
-            type:String,
-            default:"pending"
-        }
+  {
+    orderId: {
+      type: String,
+      default: () => Math.floor(100000 + Math.random() * 900000).toString(),
+      unique: true,
     },
-    {timestamps:true}
-)
+    user: {
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      username: {
+        type: String,
+        ref: "User",
+      },
+    },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    status: {
+      type: String,
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
 module.exports = mongoose.model("Order",orderSchema)
